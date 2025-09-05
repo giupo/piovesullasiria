998bdd34c3e71b2d60fa647f345790fd107133c4
MasterOfPuppets <giuseppe.acito@bancaditalia.it>
2025-09-05 22:28:33 +0200

    adds mean_metar

--- api.py
+++ api.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter
+
+from scenery import Scenery
+from weather import mean_metar
+
+from typing import Dict, Optional
 
 router = APIRouter(prefix="/api")
 
@@ -7,3 +12,7 @@ def hello_world():
     return {
         "msg": "Hello World!"
     }
+
+@router.get("/metar/{scenery}")
+def get_mean_metar(scenery: Scenery) -> Optional[Dict]
+    return mean_metar(scenery = scenery)
